18103c0607b2ecd2691999e41f7379cceafe0d2f
gitrlyn <54748346+gitrlyn@users.noreply.github.com>
2019-12-10 21:59:04 -0600

    Got passing the route parameters to the cat component working!
    the get(name) is where it's at.

--- src/app/cat/cat.component.ts
+++ src/app/cat/cat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cat',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cat.component.css']
 })
 export class CatComponent implements OnInit {
-
-  constructor() { }
+  name = "Don't gots a name";
+  // dependency inject (DI) the ActivatedRoute into the cat component
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    // let name = this.activatedRoute.snapshot.paramMap.get('name');
   }
 
+  // create readonly (getter) property that looks up the name passed on the route - use Snapshot method in Angular docs
+  get(name) {
+    return this.activatedRoute.snapshot.params.name;
+  }
 }
